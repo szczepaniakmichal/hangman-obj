74d49f89137673b567b8705e1bbad00a642cdba3
szczepaniakmichal <mssk87@gmail.com>
2020-09-29 22:41:02 +0200

    upgrade generatePassword(), generateKeyboard(), add clean or gulp

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -1,64 +1,74 @@
-class Game {
+import { Quote } from './Quote.js'
 
-    constructor({imgWraper, passwordWraper, categorywraper, keybordwraper}) {
-        this.imgWraper = imgWraper;
-        this.passwordWraper = passwordWraper;
-        this.categoryWraper = categorywraper;
-        this.keybordWraper = keybordwraper;
+class Game {
+    constructor({imgWrapper, passwordWrapper, categoryWrapper, keyboardWrapper}) {
+        this.imgWrapper = imgWrapper;
+        this.passwordWrapper = passwordWrapper;
+        this.categoryWrapper = categoryWrapper;
+        this.keyboardWrapper = keyboardWrapper;
+
+        this.quotes = [
+            {
+                text: "test ",
+                category: "film",
+            },
+            {
+                text: "test test",
+                category: "gra",
+            },
+            {
+                text: "test test test",
+                category: "motoryzacja",
+            },
+            {
+                text: "test test test test",
+                category: "motoryzacja",
+            },
+            {
+                text: "test test test test test ",
+                category: "motoryzacja",
+            },
+            {
+                text: "test test test test test test",
+                category: "motoryzacja",
+            }
+        ];
     }
 
-    quotes = [
-        {
-            text: "test jeden",
-            category: "film",
-        },
-        {
-            text: "test dwa",
-            category: "gra",
-        },
-        {
-            text: "test trzy",
-            category: "motoryzacja",
-        }
-    ];
-
-
     generatePassword() {
-        console.log("this.quotes", this.quotes)
         const randomNumber = Math.floor(Math.random() * this.quotes.length);
-        const { text, category } = this.quotes;
-        console.log(text)
-        this.passwordWraper.textContent = text;
-        this.categoryWraper.textContent = category;
+        const {text, category} = this.quotes[randomNumber];
+        this.quote = new Quote(text);
+        this.categoryWrapper.textContent = category;
     }
 
     guess(letter) {
         console.log(letter)
     }
 
-    generateKeybord() {
+    generateKeyboard() {
         for (let i = 0; i < 26; i++) {
             const letter = (i + 10).toString(36);
             const btn = document.createElement('button');
             btn.classList.add('letterBtn');
             btn.textContent = letter;
             btn.addEventListener('click', () => this.guess(letter));
-            this.keybordWraper.appendChild(btn);
+            this.keyboardWrapper.appendChild(btn);
         }
     }
 
+
     start() {
         this.generatePassword();
-        this.generateKeybord();
+        this.generateKeyboard();
     }
-
 }
 
 const game = new Game({
-    imgWraper: document.querySelector('.game-container__img'),
-    passwordWraper: document.querySelector('.game-container__password'),
-    categorywraper: document.querySelector('.game-container__category'),
-    keybordwraper: document.querySelector('.game-container__keybord'),
+    imgWrapper: document.querySelector('.game-container__img'),
+    passwordWrapper: document.querySelector('.game-container__password'),
+    categoryWrapper: document.querySelector('.game-container__category'),
+    keyboardWrapper: document.querySelector('.game-container__keyboard'),
 });
 
 game.start();
